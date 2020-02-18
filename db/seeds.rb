3091e212001da2b68474f92874c48269ba3b815d
Piotr Damer <piotrd@czuby.net>
2020-02-18 12:04:38 +0000

    Built a basic task list

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+15.times do
+  task = Task.new(title: Faker::Beer.brand, details: Faker::ChuckNorris.fact, completed: false)
+  task.save
+end
